15123f550033cc4213567fea6f64109d0eb913df
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-31 00:32:37 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,18 @@
 class Cat
-  attr_accessor :mood
   attr_reader :name
+  attr_accessor :owner, :mood
 
-  def initialize(name)
+  @@all = []
+
+  def initialize(name, owner, mood="nervous")
     @name = name
-    @mood = "nervous"
+    @owner = owner
+    @mood = mood
+    @@all << self
+    @owner.new_cat(self)
+  end
+
+  def self.all
+    @@all
   end
 end
